b9fbc20a54f5ff07ba20d5bee073876e96350f88
BryzeTheGreat <beverlylisondra12345@gmail.com>
2026-05-12 08:47:10 +0800

    Initialize SQLAlchemy connection with SQLite

---
+++ Connections/connect.py
@@ -0,0 +1,10 @@
+from sqlalchemy import create_engine, Column, String, Integer
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+engine = create_engine("sqlite:///test.db")
+
+Base = declarative_base()
+
+Session = sessionmaker(engine)
+
+session = Session()  
